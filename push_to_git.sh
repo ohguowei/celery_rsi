d3dcf42761fb18c2204db76ee543eb898e46b85b
ohguowei <ohguowei@hotmail.com>
2023-09-10 14:18:44 +0800

    test push

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,5 +1,5 @@
-git remote add origin https://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@github.com/ohguowei/rsi.git
+git remote add origin https://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@github.com/ohguowei/celery_rsi
 git add .
 git commit -m "test push"
 git branch -M main
-git push -u origin main
+git push -u origin master
