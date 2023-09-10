b136e5e4e652fc9d006e11a923faef9c60f29579
ohguowei <ohguowei@hotmail.com>
2023-09-10 14:26:55 +0800

    test push

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the remote URL with the token
-git remote set-url origin https://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@github.com/ohguowei/celery_rsi.git
+git remote set-url origin https://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@github.com/ohguowei/celery_rsi.git
 
 # Add all changes to the staging area
 git add .
