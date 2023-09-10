28ef3966f976dd9d9e286c6766b0139572e35168
ohguowei <ohguowei@hotmail.com>
2023-09-10 14:20:01 +0800

    test push

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,5 +1,14 @@
-git remote add origin https://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@github.com/ohguowei/celery_rsi
+#!/bin/bash
+
+# Set the remote URL with the token
+git remote set-url origin https://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@github.com/ohguowei/celery_rsi.git
+
+# Add all changes to the staging area
 git add .
+
+# Commit the changes
 git commit -m "test push"
-git branch -M main
-git push -u origin master
+
+# Push the changes to the main branch on the remote repository
+git push -u origin main
+
