a0b2c28f391877769cd1c32bfde1ea00a6dbc2ce
ohguowei <ohguowei@hotmail.com>
2023-09-11 12:18:15 +0800

    Update pull_github.sh

--- pull_github.sh
+++ pull_github.sh
@@ -12,27 +12,26 @@ cd $GIT_DIR
 # Store the current commit hash
 CURRENT_COMMIT=$(git rev-parse HEAD)
 
-# Check for local changes
-LOCAL_CHANGES=$(git diff --name-only)
-
 # Pull the latest changes from the repository
 git pull origin master
 
-# Check if the commit has changed after pulling
-if [[ "$CURRENT_COMMIT" != $(git rev-parse HEAD) || -n "$LOCAL_CHANGES" ]]; then
+# Check for changes between the old commit and the new commit
+CHANGED_FILES=$(git diff --name-only $CURRENT_COMMIT)
+
+# Check if there were any changes
+if [[ "$CURRENT_COMMIT" != $(git rev-parse HEAD) ]]; then
     # If the hostname is rbot1, rbot2, or rbotz1
     if [[ "$HOSTNAME" == "rbot1" || "$HOSTNAME" == "rbot2" || "$HOSTNAME" == "rbotz1" ]]; then
         # Restart the celery.service
         sudo systemctl restart celery.service
     fi
 
-    # If the hostname is rbot1 or rbot2 and celery_config.py has changed
+    # If the hostname is rbot1 or rbot2
     if [[ "$HOSTNAME" == "rbot1" || "$HOSTNAME" == "rbot2" ]]; then
-        # Check if celery_config.py is changed and if celerybeat.service is running
-        if (echo "$LOCAL_CHANGES" | grep "celery_config.py" || git diff --name-only $CURRENT_COMMIT | grep "celery_config.py") && systemctl is-active --quiet celerybeat.service; then
+        # Check if celery_config.py has changed and if celerybeat.service is running
+        if echo "$CHANGED_FILES" | grep -q "celery_config.py" && systemctl is-active --quiet celerybeat.service; then
             # Restart the celerybeat.service
             sudo systemctl restart celerybeat.service
         fi
     fi
 fi
-
